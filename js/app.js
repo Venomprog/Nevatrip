872dce45a3304435a0dfa82ae7a5c16ad7de26c7
Shaev Gleb <75539098+Venomprog@users.noreply.github.com>
2022-10-20 22:54:34 +0300

    comm

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 "use strict";
 
 const moreTimeBtn = document.querySelector('.voyage-add-btn');
+const closeTimeBtn = document.querySelector('.voyage-close-btn')
 const timeBlock = document.querySelector('.more-time');
 
 moreTimeBtn.addEventListener('click', () =>{
-    timeBlock.classList.add('hidden-list');
+    timeBlock.classList.remove('hidden-list'); //При нажатии показывает доп. время
+    closeTimeBtn.classList.remove('hidden-list');
+    moreTimeBtn.classList.add('hidden-list');
+
+});
+closeTimeBtn.addEventListener('click', () =>{
+    timeBlock.classList.add('hidden-list'); //При нажатии убирает доп. время
+    closeTimeBtn.classList.add('hidden-list');
+    moreTimeBtn.classList.remove('hidden-list');
 });
