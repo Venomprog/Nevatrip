caf9a1768580caec8bf736e96f1d2f7765f7cb39
Shaev Gleb <75539098+Venomprog@users.noreply.github.com>
2022-10-22 21:28:59 +0300

    btn works correct

--- js/app.js
+++ js/app.js
@@ -3,31 +3,28 @@ const moreTimeBtns = document.querySelectorAll('.voyage-add-btn');
 const closeTimeBtns = document.querySelectorAll('.voyage-close-btn');
 const timeBlocks = document.querySelectorAll('.more-time');
 
-function openBlock(item){
+function openBlock(item, num){
     item.classList.add('hidden-list');
-    timeBlocks.forEach(item =>{
-        item.classList.remove('hidden-list');
-    });
+    timeBlocks[num].classList.remove('hidden-list');
+    closeTimeBtns[num].classList.remove('hidden-list');
 }
 
-function closeBlock(item){
+function closeBlock(item, num){
     item.classList.add('hidden-list');
-    timeBlocks.forEach(item =>{
-        item.classList.add('hidden-list');
-    });
+    timeBlocks[num].classList.add('hidden-list');
+    moreTimeBtns[num].classList.remove('hidden-list');
 }
 
 moreTimeBtns.forEach(item =>{
     item.addEventListener('click', (e) => {
-        openBlock(e.target);
+        const num = e.target.id;
+        openBlock(e.target, num);
     });
 });
 
 closeTimeBtns.forEach(item =>{
     item.addEventListener('click', (e) => {
-        closeBlock(e.target);
+        const num = e.target.id;
+        closeBlock(e.target, num);
     });
 });
-
-
-//Доработать кнопки правильно!
