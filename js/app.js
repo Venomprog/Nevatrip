9e5fb1239abc072ab3506a43a160f4c5804df6e2
Shaev Gleb <75539098+Venomprog@users.noreply.github.com>
2022-10-23 22:22:55 +0300

    comment

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function closeBlock(item, num){
 
 moreTimeBtns.forEach(item =>{
     item.addEventListener('click', (e) => {
-        const num = e.target.id;  //получение айдишника с цели клика
+        const num = e.target.id;  //получение айдишника с цели клика12
         openBlock(e.target, num); //передача номера айди и самой цели клика1
     });
 });
